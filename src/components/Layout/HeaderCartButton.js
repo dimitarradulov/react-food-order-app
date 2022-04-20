b06b7d04040e329c218cc26901c6c08cf5ba64b7
Dimitar Radulov <dimitaradulov@gmail.com>
2022-04-20 10:23:14 +0300

    Add context for amount display

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,19 @@
+import { useContext } from 'react';
+
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
   return (
     <button onClick={props.onShowCart} className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{cartCtx.totalAmount}</span>
     </button>
   );
 };
