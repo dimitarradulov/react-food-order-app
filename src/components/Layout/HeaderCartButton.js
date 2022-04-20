bcc0f603203ef95b24048a20a246da84f19ccbdc
Dimitar Radulov <dimitaradulov@gmail.com>
2022-04-20 11:09:17 +0300

    Change the badge display logic

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -7,13 +7,17 @@ import CartContext from '../../store/cart-context';
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
 
+  const totalAmount = cartCtx.items.reduce((prevValue, item) => {
+    return prevValue + item.amount;
+  }, 0);
+
   return (
     <button onClick={props.onShowCart} className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>{cartCtx.totalAmount}</span>
+      <span className={classes.badge}>{totalAmount}</span>
     </button>
   );
 };
