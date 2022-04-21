04f19399f55a5399c8fc683ab13be25ea07b16c6
Dimitar Radulov <dimitaradulov@gmail.com>
2022-04-21 09:54:35 +0300

    Add cart item logic for adding & removing quantity

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,9 +11,13 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemove = (id) => {};
+  const cartItemRemove = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const cartItemAdd = (item) => {};
+  const cartItemAdd = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes['cart-items']}>
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -37,6 +37,37 @@ const cartReducer = (state, action) => {
     };
   }
 
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingItem = state.items[existingCartItemIndex];
+
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+    let updatedItems = [...state.items];
+
+    if (existingItem.amount === 1) {
+      updatedItems.splice(existingCartItemIndex, 1);
+
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
+      };
+    }
+
+    updatedItems[existingCartItemIndex] = {
+      ...existingItem,
+      amount: existingItem.amount - 1,
+    };
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
   return defaultCartState;
 };
 
