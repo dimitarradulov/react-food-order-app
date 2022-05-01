89f4314e38882ba3197e4b70cd973cab76f04de0
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-05-01 16:35:52 +0300

    Add fetching with useFetch custom hook

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,50 +1,56 @@
+import { useState, useEffect } from 'react';
+
+import useFetch from '../../hooks/use-fetch';
 import classes from './AvailableMeals.module.css';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem
-      key={meal.id}
-      id={meal.id}
-      name={meal.name}
-      description={meal.description}
-      price={meal.price}
-    />
-  ));
+  const [mealsData, setMealsData] = useState([]);
+  const { errorOccured, isLoading, httpRequest } = useFetch();
+
+  const applyDataTransformation = function (data) {
+    const transformedData = [];
+
+    Object.keys(data).forEach((mealKey) => {
+      transformedData.push({ id: mealKey, ...data[mealKey] });
+    });
+
+    setMealsData(transformedData);
+  };
+
+  useEffect(() => {
+    httpRequest(
+      {
+        url: 'https://custom-hooks-9ea7a-default-rtdb.europe-west1.firebasedatabase.app/meals.json',
+      },
+      applyDataTransformation
+    );
+  }, [httpRequest]);
+
+  let content = <p>Loading...</p>;
+
+  if (!isLoading && !errorOccured) {
+    const mealsList = mealsData.map((meal) => (
+      <MealItem
+        key={meal.id}
+        id={meal.id}
+        name={meal.name}
+        description={meal.description}
+        price={meal.price}
+      />
+    ));
+
+    content = <ul>{mealsList}</ul>;
+  }
+
+  if (errorOccured) {
+    content = <p>{errorOccured}</p>;
+  }
 
   return (
     <section className={classes.meals}>
-      <Card>
-        <ul>{mealsList}</ul>
-      </Card>
+      <Card>{content}</Card>
     </section>
   );
 };
