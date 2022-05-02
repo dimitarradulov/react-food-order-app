0b53e3eeb1b0d31d59efa5ca4a7121e4cd8d79ca
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-05-02 11:43:00 +0300

    Add clear cart function

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -91,11 +91,16 @@ const CartProvider = (props) => {
     });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartState({ type: 'CLEAR' });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return (
