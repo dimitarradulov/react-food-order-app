3587dd278c2eab1d2bd968d11b4955b7ae815445
Dimitar Radulov <76252856+dimitarradulov@users.noreply.github.com>
2022-05-02 11:42:51 +0300

    Add order submit to backend

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,6 @@
-import { useContext, useState } from 'react';
+import { useContext, useState, Fragment } from 'react';
 
+import useFetch from '../../hooks/use-fetch';
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
@@ -8,6 +9,7 @@ import Checkout from './Checkout';
 
 const Cart = (props) => {
   const [isCheckout, setIsCheckout] = useState(false);
+  const { errorOccured, isLoading, httpRequest, didSubmit } = useFetch();
 
   const cartCtx = useContext(CartContext);
 
@@ -24,6 +26,20 @@ const Cart = (props) => {
 
   const onOrderHandler = () => {
     setIsCheckout(true);
+  };
+
+  const submitOrderHandler = (userData) => {
+    httpRequest(
+      {
+        url: 'https://custom-hooks-9ea7a-default-rtdb.europe-west1.firebasedatabase.app/orders.json',
+        method: 'POST',
+        body: {
+          user: userData,
+          orderedItems: cartCtx.items,
+        },
+      },
+      cartCtx.clearCart
+    );
   };
 
   const cartItems = (
@@ -54,15 +70,50 @@ const Cart = (props) => {
     </div>
   );
 
-  return (
-    <Modal onHideCart={props.onHideCart}>
+  const initialModalContent = (
+    <Fragment>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onHideCart} />}
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onHideCart} />
+      )}
       {!isCheckout && modalActions}
+    </Fragment>
+  );
+
+  const isLoadingContent = <p>Please wait while we process your order...</p>;
+
+  const errorContent = (
+    <Fragment>
+      <p style={{ color: 'red' }}>Error: {errorOccured}</p>
+      <div className={classes.actions}>
+        <button onClick={props.onHideCart} className={classes.button}>
+          Close
+        </button>
+      </div>
+    </Fragment>
+  );
+
+  const didSubmitContent = (
+    <Fragment>
+      <p>Your order has been received successfully!</p>
+      <div className={classes.actions}>
+        <button onClick={props.onHideCart} className={classes.button}>
+          Close
+        </button>
+      </div>
+    </Fragment>
+  );
+
+  return (
+    <Modal onHideCart={props.onHideCart}>
+      {!isLoading && !errorOccured && !didSubmit && initialModalContent}
+      {isLoading && isLoadingContent}
+      {errorOccured && errorContent}
+      {didSubmit && !errorOccured && didSubmitContent}
     </Modal>
   );
 };
